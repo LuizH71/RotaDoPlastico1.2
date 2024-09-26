ff4ef262517306e0c9de255f2a8f5dcc4882d1fd
Luiz Henrique <101363751+LuizH71@users.noreply.github.com>
2024-09-26 17:52:37 -0300

    resgate

---
+++ RotaDoPlastico/Assets/AnimalMov.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimalMov : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    public float range; //radius of sphere
+
+    private Transform _centerPoint; //ccenter of the area the agent wants to move around in
+    //instead of centrePoint you can set it as the transform of the agent if you don't care about a specific area
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        if (_agent.remainingDistance <= _agent.stoppingDistance) //done with path
+        {
+            Vector3 point;
+            if (RandomPoint(transform.position, range, out point)) //pass in our center point and radius of area
+            {
+                Debug.DrawRay(point, Vector3.up, Color.cyan, 1.0f); //so you can see with gizmos
+                _agent.SetDestination(point);
+            }
+        }
+
+    }
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+
+        Vector3 randomPoint = center + Random.insideUnitSphere * range; //random point in a sphere 
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas)) //documentation: https://docs.unity3d.com/ScriptReference/AI.NavMesh.SamplePosition.html
+        {
+            //the 1.0f is the max distance from the random point to a point on the navmesh, might want to increase if range is big
+            //or add a for loop like in the documentation
+            result = hit.position;
+            return true;
+        }
+
+        result = Vector3.zero;
+        return false;
+    }
+
+}
